8c8656e51dae684f053f87fe4e92718139fca9dd
aishaabbasim7-prog <aishaabbasim7@gmail.com>
2026-04-20 18:42:58 +0500

    Create products.js

---
+++ products.js
@@ -0,0 +1,22 @@
+const products = [
+{
+  id: "p1",
+  title: "Luxury Velvet Pillow",
+  price: "$19.99",
+  rating: "★★★★★",
+  image: "PASTE_IMAGE_URL",
+  desc: "Soft luxury velvet pillow for aesthetic home decor.",
+  link: "#",
+  keyword: "luxury velvet pillow pillow decor"
+},
+{
+  id: "p2",
+  title: "LED Smart Lamp",
+  price: "$14.99",
+  rating: "★★★★☆",
+  image: "PASTE_IMAGE_URL",
+  desc: "Modern LED lamp for bedroom and aesthetic lighting.",
+  link: "#",
+  keyword: "led lamp smart lamp lighting"
+}
+];
