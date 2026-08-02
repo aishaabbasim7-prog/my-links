772a5d4375cd8891c411a321c113422476a9eac7
aishaabbasim7-prog <aishaabbasim7@gmail.com>
2026-08-02 14:34:27 +0500

    Update makeovers.js

--- makeovers.js
+++ makeovers.js
@@ -1,1 +1,24 @@
+const makeovers = [
 
+{
+    id: "living-room-001",
+
+    title: "Budget Boho Living Room Under $150",
+
+    description: "Recreate this warm and cozy boho living room using these carefully selected Amazon home decor finds.",
+
+    products: [
+        "product1",
+        "product2",
+        "product3",
+        "product4",
+        "product5",
+        "product6",
+        "product7",
+        "product8",
+        "product9",
+        "product10"
+    ]
+}
+
+];
